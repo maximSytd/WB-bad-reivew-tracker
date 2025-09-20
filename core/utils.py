2302bf206dd073b432f95abd4e46fd5e4843379e
Maxim Sytdikov <maximovichpro@gmail.com>
2025-09-20 17:30:15 +0500

    Add tortoise_context_manager

--- core/utils.py
+++ core/utils.py
@@ -3,6 +3,9 @@ import contextlib
 
 import httpx
 import httpx_retries
+import tortoise
+
+from core.config import tortoise_settings
 
 REQUEST_TIMEOUT_SEC = 3
 MAX_RETRIES = 3
@@ -25,3 +28,14 @@ def client_manager(**kw) -> ClientManagerType:
         **kw,
     ) as c:
         yield c
+
+@contextlib.asynccontextmanager
+async def tortoise_context_manager(
+    config: dict[str, typing.Any] = tortoise_settings,
+) -> typing.AsyncGenerator[None, None]:
+    """Yield None and manage tortoise db connection."""
+    await tortoise.Tortoise.init(config=config)
+    try:
+        yield
+    finally:
+        await tortoise.Tortoise.close_connections()
